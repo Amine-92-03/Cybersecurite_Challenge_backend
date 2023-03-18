702e204df284063f28f3c0a812fc240d39ae5629
AMINE-03-92 <mefti.amine92@gmail.com>
2023-03-18 09:09:42 +0100

    ajout autres div accueil

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -1,3 +1,22 @@
+const contact = require("../Models/contact");
+
 module.exports.getHomedata = (req, res) => {
   return res.render("index", { title: "Cybersécurité Challenge" });
+};
+
+module.exports.getListViews = (req, res) => {
+  contact.User.find({})
+    .then((users) => {
+      return res.render("listeUsers", {
+        title: "Cybersécurité Challenge",
+        data: users,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "User not saved to collection!",
+        flagIntegration: false,
+        erreur: err,
+      });
+    });
 };
--- Routes/tdbRouter.js
+++ Routes/tdbRouter.js
@@ -2,5 +2,6 @@ const homeController = require("../Controllers/homeController");
 const router = require("express").Router();
 
 router.get("/", homeController.getHomedata);
+router.get("/getlistUsers", homeController.getListViews);
 
 module.exports = router;
